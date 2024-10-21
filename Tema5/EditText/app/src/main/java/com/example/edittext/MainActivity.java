2d271b6587637c69a426cc8a60d054561fe04d9e
Apedlop <angelapedreralopez@gmail.com>
2024-10-21 19:12:30 +0200

    21/10/2024, Realización Ejercicio EditText

--- Tema5/EditText/app/src/main/java/com/example/edittext/MainActivity.java
+++ Tema5/EditText/app/src/main/java/com/example/edittext/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.edittext;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Teclado telefónico 1
+        Button bt1 = (Button) findViewById(R.id.bt1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bt1 = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(bt1);
+            }
+        });
+
+        // Teclado telefónico 2
+        Button bt2 = (Button) findViewById(R.id.bt2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bt2 = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(bt2);
+            }
+        });
+
+        // Teclado alfabético
+        Button bt3 = (Button) findViewById(R.id.bt3);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bt3 = new Intent(MainActivity.this, MainActivity4.class);
+                startActivity(bt3);
+            }
+        });
+
+        // Teclado numérico
+        Button bt4 = (Button) findViewById(R.id.bt4);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bt4 = new Intent(MainActivity.this, MainActivity5.class);
+                startActivity(bt4);
+            }
         });
     }
 }
