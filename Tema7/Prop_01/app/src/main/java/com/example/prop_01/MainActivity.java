4884b10c38b43fd5b91c8b2186e02da36213e2c5
Apedlop <angelapedreralopez@gmail.com>
2024-11-29 00:46:42 +0100

    29/11/2024

--- Tema7/Prop_01/app/src/main/java/com/example/prop_01/MainActivity.java
+++ Tema7/Prop_01/app/src/main/java/com/example/prop_01/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.prop_01;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button definirPreferencias = findViewById(R.id.boton1);
+        Button recuperarPreferencias = findViewById(R.id.boton2);
+
+        // Botón para abrir la pantalla de preferencias
+        definirPreferencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PreferenciasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Botón para recuperar y mostrar las preferencias
+        recuperarPreferencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+                boolean unicoSO = prefs.getBoolean("clave1", false);
+                String sistemaOperativo = prefs.getString("sistema_operativo", "No seleccionado");
+                String version = prefs.getString("version_sistema", "No especificada");
+
+                // Mostrar los valores en el Logcat
+                Log.d("Preferencias", "Único Sistema Operativo: " + unicoSO);
+                Log.d("Preferencias", "Sistema Operativo: " + sistemaOperativo);
+                Log.d("Preferencias", "Versión del Sistema: " + version);
+            }
         });
     }
 }
