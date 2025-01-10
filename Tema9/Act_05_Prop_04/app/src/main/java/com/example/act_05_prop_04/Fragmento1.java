192cb3a38097682cff4f6ee9310acc40f9b56a2d
Apedlop <angelapedreralopez@gmail.com>
2025-01-10 11:53:40 +0100

    10/01/2025

---
+++ Tema9/Act_05_Prop_04/app/src/main/java/com/example/act_05_prop_04/Fragmento1.java
@@ -0,0 +1,11 @@
+package com.example.act_05_prop_04;
+
+import android.os.Bundle;
+import androidx.fragment.app.ListFragment;
+
+public class Fragmento1 extends ListFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
