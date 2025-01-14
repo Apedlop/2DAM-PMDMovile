a51a74482da0407b2ea01d492fb182c88a36e2e2
Apedlop <angelapedreralopez@gmail.com>
2025-01-14 13:57:00 +0100

    14/01/2025

--- Tema9/Act_05_Prop_04/app/src/main/java/com/example/act_05_prop_04/Actividad.java
+++ Tema9/Act_05_Prop_04/app/src/main/java/com/example/act_05_prop_04/Actividad.java
@@ -5,11 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentActivity;
 
 public class Actividad extends FragmentActivity implements Fragmento1.Callbacks {
--- Tema9/Act_05_Prop_04/app/src/main/java/com/example/act_05_prop_04/Contenido.java
+++ Tema9/Act_05_Prop_04/app/src/main/java/com/example/act_05_prop_04/Contenido.java
@@ -2,7 +2,6 @@ package com.example.act_05_prop_04;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Contenido {
