20ae0fdf1812d326b94f87afbd3aed956f5e11b1
Apedlop <angelapedreralopez@gmail.com>
2025-01-17 14:42:17 +0100

    17/01/2025

---
+++ Tema9/AppGestion_Fragmentos/app/src/main/java/com/example/appgestion_fragmentos/InicioSesion.java
@@ -0,0 +1,87 @@
+package com.example.appgestion_fragmentos;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+
+public class InicioSesion extends AppCompatActivity {
+
+    EditText nomUsuario, nomContraseña;
+    TextView inicioNulo;
+    Button btInicio;
+    HashMap<String, String> usuarios;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+
+        nomUsuario = findViewById(R.id.nomUsuario);
+        nomContraseña = findViewById(R.id.contraseña);
+        btInicio = findViewById(R.id.botonInicio);
+
+        // Añadir mapa de usuarios
+        usuarios = new HashMap<>();
+        usuarios.put("usuario", "usuario");
+        usuarios.put("admin", "admin");
+        usuarios.put("angela", "angela");
+
+        // Configuración del botón
+        btInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtenemos el texto ingresado en los EditText
+                String usuario = nomUsuario.getText().toString();
+                String contraseña = nomContraseña.getText().toString();
+
+                // Validar usuario y contraseña
+                if (usuarios.containsKey(usuario) && usuarios.get(usuario).equals(contraseña)) {
+                    if (usuario.equals("admin")) {
+                        // Si es admin, abrir actividad para admin
+                        Intent intent = new Intent(InicioSesion.this, ListadoMascotasAdmin.class);
+                        intent.putExtra("usuario", usuario);
+                        startActivity(intent);
+                        // Toast personalizado
+                        mostrarToast("Inicio de sesión exitoso como Admin");
+                    } else {
+                        // Si es usuario normal, abrir actividad para usuario
+                        Intent intent = new Intent(InicioSesion.this, ListadoMascotasUser.class);
+                        intent.putExtra("usuario", usuario);
+                        startActivity(intent);
+                        // Toast personalizado
+                        mostrarToast("Inicio de sesión exitoso como " + usuario);
+                    }
+                } else {
+                    // Si el usuario o la contraseña son incorrectos
+                    inicioNulo = findViewById(R.id.inicioNulo);
+                    inicioNulo.setText("Usuario o contraseña incorrectos");
+                }
+            }
+        });
+    }
+
+    // Método para mostrar toast
+    @SuppressLint("MissingInflatedId")
+    public void mostrarToast(String mensaje) {
+        // Toast personalizado
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast, (ViewGroup) findViewById(R.id.layotu_toast));
+        TextView textoToast = layout.findViewById(R.id.textoToast);
+        textoToast.setText(mensaje);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+}
---
+++ Tema9/AppGestion_Fragmentos/app/src/main/java/com/example/appgestion_fragmentos/Mascota.java
@@ -0,0 +1,102 @@
+package com.example.appgestion_fragmentos;
+
+import java.io.Serializable;
+
+public class Mascota implements Serializable {
+
+    private String nombre;
+    private String raza;
+    private int imagen;
+    private int edad;
+    private float peso;
+    private boolean vacunada;
+    private boolean desparacitada;
+    private boolean esterilizada;
+    private static int idCounter = 0;  // Contador estático para generar IDs únicos
+
+    public Mascota(String nombre, String raza, int imagen, int edad, float peso, boolean vacunada, boolean desparacitada, boolean esterilizada) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.imagen = imagen;
+        this.edad = edad;
+        this.peso = peso;
+        this.vacunada = vacunada;
+        this.desparacitada = desparacitada;
+        this.esterilizada = esterilizada;
+    }
+
+    public Mascota(String nombre, String raza, int edad, float peso, boolean vacunada, boolean desparacitada, boolean esterilizada) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.peso = peso;
+        this.vacunada = vacunada;
+        this.desparacitada = desparacitada;
+        this.esterilizada = esterilizada;
+    }
+
+    // Métodos getter y setter para cada propiedad
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public boolean isVacunada() {
+        return vacunada;
+    }
+
+    public void setVacunada(boolean vacunada) {
+        this.vacunada = vacunada;
+    }
+
+    public boolean isDesparacitada() {
+        return desparacitada;
+    }
+
+    public void setDesparacitada(boolean desparacitada) {
+        this.desparacitada = desparacitada;
+    }
+
+    public boolean isEsterilizada() {
+        return esterilizada;
+    }
+
+    public void setEsterilizada(boolean esterilizada) {
+        this.esterilizada = esterilizada;
+    }
+}
